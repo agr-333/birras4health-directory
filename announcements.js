4e50b9bf88d0d047c15244dcb70347dbaa7df902
Antonio Garcia <159025721+agr-333@users.noreply.github.com>
2026-07-17 16:35:10 +0200

    anuncio: Evento — Birras for Health @Taproom

--- announcements.js
+++ announcements.js
@@ -1,1 +1,13 @@
-window.BIRRAS4HEALTH_ANNOUNCEMENTS = [];
+window.BIRRAS4HEALTH_ANNOUNCEMENTS = [
+  {
+    "id": "1784297037858",
+    "type": "Evento",
+    "title": "Birras for Health @Taproom",
+    "description": "Quedada en Taproom Chamberí a las 18:00",
+    "url": "https://taproommadrid.com/",
+    "image": "",
+    "date": "2026-07-21T22:00:00.000Z",
+    "author": "Antonio García Romero",
+    "created": "2026-07-16T22:00:00.000Z"
+  }
+];
