479418419ce43877eb8f70aca4f8ac2d62c162ef
Antonio Garcia <159025721+agr-333@users.noreply.github.com>
2026-07-17 11:46:18 +0200

    anuncio: Evento — Nueva quedada B4H

--- announcements.js
+++ announcements.js
@@ -1,1 +1,13 @@
-window.BIRRAS4HEALTH_ANNOUNCEMENTS = [];
+window.BIRRAS4HEALTH_ANNOUNCEMENTS = [
+  {
+    "id": "1784281577621",
+    "type": "Evento",
+    "title": "Nueva quedada B4H",
+    "description": "Segundo evento B4H en Taproom (Guzmán el Bueno) 18:00",
+    "url": "https://taproommadrid.com/",
+    "image": "",
+    "date": "2026-07-22",
+    "author": "Antonio García Romero",
+    "created": "2026-07-17"
+  }
+];
