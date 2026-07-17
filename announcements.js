af7630676e57201e82797629bb3ed7b885c35d77
Antonio Garcia <159025721+agr-333@users.noreply.github.com>
2026-07-17 12:14:47 +0200

    anuncio: Noticia — IE

--- announcements.js
+++ announcements.js
@@ -1,5 +1,16 @@
 window.BIRRAS4HEALTH_ANNOUNCEMENTS = [
   {
+    "id": "1784283286898",
+    "type": "Noticia",
+    "title": "IE",
+    "description": "",
+    "url": "ie.edu",
+    "image": "https://images.thesaurus.ie.edu/f/jpg/w/1280/h/768/ie-university-logo_1",
+    "date": "",
+    "author": "Antonio García Romero",
+    "created": "2026-07-17"
+  },
+  {
     "id": "1784281577621",
     "type": "Evento",
     "title": "Nueva quedada B4H",
